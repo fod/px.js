6731864db9d64da6ce7f4e59d664750d0d915d42
Fiachra O'Donoghue <fodonoghue@gmail.com>
2012-08-16 03:12:57 +0100

    dataCol fn renamed and changed so as not to alter passed array

--- test/use-test/node-use-test.js
+++ test/use-test/node-use-test.js
@@ -1,20 +1,14 @@
 GLOBAL._ = require('underscore');
 var Px = require('../../lib/px.js'),
-    fs = require('fs'),
-    iconv = require('iconv-lite');
+    fs = require('fs');
 
-var pxfile = '../testData/020101.px';
+var pxfile = '../testData/VSA31.px';
 
 fs.readFile(pxfile, 'binary', function(err, data) {
     if (err) { throw err; }
     console.log('\Loading ' + pxfile + '...');
-    var decoded = iconv.decode(data, 'Windows-1251');
-    px = new Px(decoded);
-    console.log(px.variables());
-    var v1 = px.variable(0);
-    console.log(v1);
-    var val = px.values(v1)[px.values(v1).length-1];
-    console.log(px.code(val, v1));
+    px = new Px(data);
+console.log(px.explicitDatatable());
 });
 
 
