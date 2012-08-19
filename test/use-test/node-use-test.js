820e4c61e5909f6d70243f82847818576ae6a131
Fiachra O'Donoghue <fodonoghue@gmail.com>
2012-08-19 06:53:15 +0100

    Fixed underscore require bug under node

--- test/use-test/node-use-test.js
+++ test/use-test/node-use-test.js
@@ -1,14 +1,14 @@
-GLOBAL._ = require('underscore');
+//_ = require('underscore');
 var Px = require('../../lib/px.js'),
     fs = require('fs');
 
 var pxfile = '../testData/VSA31.px';
 
-fs.readFile(pxfile, 'binary', function(err, data) {
+fs.readFile(pxfile, 'utf8', function(err, data) {
     if (err) { throw err; }
     console.log('\Loading ' + pxfile + '...');
     px = new Px(data);
-console.log(px.explicitDatatable());
+console.log(px.variables());
 });
 
 
--- test/use-test/use-test.js
+++ test/use-test/use-test.js
@@ -1,20 +1,32 @@
-$(function() {
+//$(function() {
 
     document.getElementById('pxfile').onchange = handlePxfile;
 
-    window.px = {};
+    var px = {};
     function handlePxfile() {
 
-	var reader = new FileReader();
-	var file = this.files[0].name.replace('.px', '');
-	reader.onload = function() { return px[file] = new Px(reader.result); };
-	reader.onerror = function () {
-	    console.log('FileReader error loading: ' + file + '.px. Are you running your app from file:// ? FileReader API does not allow running from file:// for security reasons. See https://developer.mozilla.org/en/DOM/File_API/File_System_API/FileError for more information.');
-	};
-	reader.readAsText(this.files[0], 'cp1251');
+    	var reader = new FileReader();
+    	var file = this.files[0].name.replace('.px', '');
+
+    	reader.onload = function() { return px[file] = new Px(reader.result); };
+    	reader.onerror = function () {
+    	    console.log('FileReader error loading: ' + file + '.px. Are you running your app from file:// ? FileReader API does not allow running from file:// for security reasons. See https://developer.mozilla.org/en/DOM/File_API/File_System_API/FileError for more information.');
+    	};
+    	reader.readAsText(this.files[0], 'cp1251');
     }
+//});
 
-});
 
+// var xhr = new XMLHttpRequest();
+
+// xhr.onreadystatechange = function() {
+//     if (xhr.readyState === 4 && xhr.status === 200) {
+// 	console.log(xhr.responseText);
+// 	var px = new Px(xhr.responseText);
+//     }
+// };
+
+// xhr.open('GET', '../testData/VSA31.px');
+// xhr.send();
 
 
